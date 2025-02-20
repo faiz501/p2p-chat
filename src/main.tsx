e1c487f7bb9b86f150e1d811ffc8de0d994df967
Apurva Mishra <apurva.jpr@gmail.com>
2025-02-20 13:46:31 +0530

    Update Basic UI - Create Room - Join Room - Send Message - Receive Message

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,14 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <button>Create Room</button>
+    <p id="crt_rm"></p>
+    <input type="text" id="join" placeholder="Join"></input>
+    <button>Join Room</button>
+    <input type="text" id="snd_mdg" placeholder="Send Message"></input>
+    <button>Send Message</button>
+    <br />
+    <p id="rcv_mdg"></p>
     <App />
   </React.StrictMode>,
 );
